1f032a0e558e52efaee551ca6eddfb2fc6fe05a2
Marcin Bloch <43933801+Marcinbloch1@users.noreply.github.com>
2022-06-08 20:27:09 +0200

    Added load directory output image and rearranged layout

--- image.py
+++ image.py
@@ -1,14 +1,19 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QFileDialog
+from os import mkdir
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QFileDialog, QTextEdit
 from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
+from distutils.dir_util import copy_tree
 import sys
 
 IMAGE_WIDTH = 720
 IMAGE_HEIGHT = 471
+TRAIN_DIRECTORY_PATH = r"C:\Users\Marcin\FaceNet-GUI\FaceNet-GUIApp\\"
+OUTPUT_FILE_DIRECTORY_PATH = r"C:\Users\Marcin\FaceNet-GUI\FaceNet-GUIApp\\"
 
 class UI(QMainWindow):
 	def __init__(self):
 		self.fname = None
+		self.outputFileName = None
 		super(UI, self).__init__()
 
 		# Load the ui file
@@ -17,11 +22,16 @@ class UI(QMainWindow):
 		# Define our widgets
 		self.button = self.findChild(QPushButton, "pushButton")
 		self.button_2 = self.findChild(QPushButton, "pushButton_2")
+		self.button_3 = self.findChild(QPushButton, "pushButton_3")
 		self.label = self.findChild(QLabel, "label")
+		self.label_3 = self.findChild(QLabel, "label_3")
+		self.textEdit = self.findChild(QTextEdit, "textEdit")
 
 		# Click The Dropdown Box
 		self.button.clicked.connect(self.loadImage)
 		self.button_2.clicked.connect(self.processImage)
+		self.button_3.clicked.connect(self.loadDirectory)
+		
 
 		# Show The App
 		self.show()
@@ -37,7 +47,19 @@ class UI(QMainWindow):
 
 	def processImage(self):
 		print(self.fname)
-		self.label_2.setText('Ryj kolegi')
+		self.label_3.setPixmap(self.pixmap)
+	
+	def loadDirectory(self):
+		dir_ = QFileDialog.getExistingDirectory(None, 'Select a folder:', 'C:\\', QFileDialog.ShowDirsOnly)
+
+		print(dir_)
+		print(self.textEdit.toPlainText())
+
+		path = TRAIN_DIRECTORY_PATH + self.textEdit.toPlainText()
+		print(path)
+
+		mkdir(path)
+		copy_tree(dir_, path)
 
 # Initialize The App
 app = QApplication(sys.argv)
